6a573bfcac7d6543109a79a16d07dbee3ce70ad7
unknown <xvalen214x@gmail.com>
2020-08-05 23:46:58 +0800

    :recycle: separated babylon script load logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,8 @@
 <script lang="ts">
   import { startGame, getFps } from "./Game";
-  import type { LoadedFunction } from "./TaskScheduler";
-  import { waitFor } from "./TaskScheduler";
+  import { scriptLoadPromise } from "./BabylonJSLoader.svelte";
   
 	export let name: string;
-  
-  const BABYLON_BASE = "https://preview.babylonjs.com";
-  const BABYLON_SCRIPT = [
-    "https://code.jquery.com/pep/0.4.2/pep.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/" +
-        "dat-gui/0.6.2/dat.gui.min.js",
-    `${BABYLON_BASE}/ammo.js`,
-    `${BABYLON_BASE}/cannon.js`,
-    `${BABYLON_BASE}/Oimo.js`,
-    `${BABYLON_BASE}/libktx.js`,
-    `${BABYLON_BASE}/earcut.min.js`,
-    `${BABYLON_BASE}/babylon.js`,
-    `${BABYLON_BASE}/inspector/babylon.inspector.bundle.js`,
-    `${BABYLON_BASE}/materialsLibrary/babylonjs.materials.min.js`,
-    `${BABYLON_BASE}/proceduralTexturesLibrary/babylonjs.proceduralTextures.min.js`,
-    `${BABYLON_BASE}/postProcessesLibrary/` +
-        `babylonjs.postProcess.min.js`,
-        `${BABYLON_BASE}/loaders/babylonjs.loaders.js`,
-    `${BABYLON_BASE}/serializers/babylonjs.serializers.min.js`,
-    `${BABYLON_BASE}/gui/babylon.gui.min.js`,
-  ];
   
   let started = true;
   let fps = "";
@@ -32,58 +10,21 @@
     fps = getFps();
   }, 500);
 
-  let [
-      loadCanvas, 
-      ...babylonLoaded
-  ] = waitFor(BABYLON_SCRIPT.length + 1,
-  (canvas, _, _1, gui) => {
-    console.log("all resource loaded, starting game");
-    console.log(gui);
-    startGame(canvas);
-  })
 
   // "tick-less" approach
   let canvasContainer = Object.defineProperty({},
     "canvas", {
-    set(value){
-      loadCanvas(value);
+    async set(value){
+      await scriptLoadPromise;
+      console.log("all resource loaded, starting game");
+      startGame(value);
+      
     }
   });
-
-  const BABYLON_LOADER = BABYLON_SCRIPT.map((url, i) => [
-    url, babylonLoaded[i]
-  ]);
-  (async () => {
-    function injectScript(src, onload){
-      let head = document.getElementsByTagName("head")[0];
-      let script = document.createElement("script");
-      script.src = src;
-      script.onload = onload;
-      head.appendChild(script);
-    }
-
-    for(var i = 0; i < BABYLON_LOADER.length; ++i){
-      await new Promise(res => {
-        let [ src, onload ] = BABYLON_LOADER[i];
-        injectScript(src, () => {
-          (onload as LoadedFunction)();
-          res();
-        });
-      }
-      );
-    }
-  })();
 
 
 </script>
 
-<svelte:head>
-  <!--
-	{#each BABYLON_LOADER as [ src, load ]}
-    <script src={src} on:load={load}></script>
-  {/each}
-  -->
-</svelte:head>
 
 
 <main>
---
+++ src/BabylonJSLoader.svelte
@@ -0,0 +1,61 @@
+<script>
+import { waitFor } from "./TaskScheduler";
+
+</script>
+<script lang="ts" context="module">
+  
+  function injectScript(src, onload){
+    let head = document.getElementsByTagName("head")[0];
+    let script = document.createElement("script");
+    script.src = src;
+    script.onload = onload;
+    head.appendChild(script);
+  }
+
+
+  const BABYLON_BASE = "https://preview.babylonjs.com";
+  const BABYLON_SCRIPT = [
+    "https://code.jquery.com/pep/0.4.2/pep.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/" +
+        "dat-gui/0.6.2/dat.gui.min.js",
+    `${BABYLON_BASE}/ammo.js`,
+    `${BABYLON_BASE}/cannon.js`,
+    `${BABYLON_BASE}/Oimo.js`,
+    `${BABYLON_BASE}/libktx.js`,
+    `${BABYLON_BASE}/earcut.min.js`,
+  ];
+  const BABYLON_MAIN_SCRIPT = `${BABYLON_BASE}/babylon.js`;
+  const BABYLON_DEPENDENT_SCRIPT = [
+    `${BABYLON_BASE}/inspector/babylon.inspector.bundle.js`,
+    `${BABYLON_BASE}/materialsLibrary/babylonjs.materials.min.js`,
+    `${BABYLON_BASE}/proceduralTexturesLibrary/babylonjs.proceduralTextures.min.js`,
+    `${BABYLON_BASE}/postProcessesLibrary/` +
+        `babylonjs.postProcess.min.js`,
+        `${BABYLON_BASE}/loaders/babylonjs.loaders.js`,
+    `${BABYLON_BASE}/serializers/babylonjs.serializers.min.js`,
+    `${BABYLON_BASE}/gui/babylon.gui.min.js`,
+  ];
+
+  Promise.all([...BABYLON_SCRIPT.map(src => (
+    new Promise(res => {
+      injectScript(src, res)
+    })
+  )), new Promise(res => (
+    injectScript(BABYLON_MAIN_SCRIPT, res)
+  )).then(() => Promise.all(BABYLON_DEPENDENT_SCRIPT.map(
+    src => new Promise(res => {
+      injectScript(src, res)
+    })
+  )))])
+
+
+</script>
+
+
+<svelte:head>
+  <!--
+	{#each BABYLON_LOADER as [ src, load ]}
+    <script src={src} on:load={load}></script>
+  {/each}
+  -->
+</svelte:head>
