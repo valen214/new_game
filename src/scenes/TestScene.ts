90962312fce1081ff55cc93993e012e8aaa1adf7
unknown <xvalen214x@gmail.com>
2020-08-14 03:37:59 +0800

    update

--- src/scenes/TestScene.ts
+++ src/scenes/TestScene.ts
@@ -15,6 +15,8 @@ export class TestScene
 extends BABYLON.Scene
 implements IScene
 {
+  player: Human;
+  pressed = new Set<string>();
   constructor(
       engine: BABYLON.Engine,
       options?: BABYLON.SceneOptions
@@ -28,81 +30,20 @@ implements IScene
 
   addEventListeners(){
 
-    let pressed = new Set();
-
-    this.onKeyboardObservable.add(({ type, event}) => {
+    this.onKeyboardObservable.add(({ type, event }) => {
       console.assert(type === BABYLON.KeyboardEventTypes.KEYDOWN);
-      pressed.add(event.code);
+      this.pressed.add(event.code);
     }, BABYLON.KeyboardEventTypes.KEYDOWN);
     
-    this.onKeyboardObservable.add(({ type, event}) => {
+    this.onKeyboardObservable.add(({ type, event }) => {
       console.assert(type === BABYLON.KeyboardEventTypes.KEYUP);
-      pressed.delete(event.code);
+      this.pressed.delete(event.code);
     }, BABYLON.KeyboardEventTypes.KEYUP);
 
     this.onPointerObservable.add(() => {
 
     });
 
-    let last_frame_time = performance.now();
-    this.onBeforeRenderObservable.add(() => {
-      let elapsed = performance.now() - last_frame_time;
-      let elapsed_sec = elapsed * 0.001;
-      last_frame_time = performance.now();
-
-      let c = this.activeCamera.getForwardRay().direction;
-      let norm_c = new BABYLON.Vector2(c.x, c.z).normalize();
-
-      let move = BABYLON.Vector2.Zero();
-      if(pressed.has("KeyA")){ move.x -= 1; }
-      if(pressed.has("KeyD")){ move.x += 1; }
-      if(pressed.has("KeyW")){ move.y += 1; }
-      if(pressed.has("KeyS")){ move.y -= 1; }
-      move.normalize().scaleInPlace(elapsed_sec * 5);
-
-
-      let player = this.getMeshByName("player");
-      if(player){
-        player.moveWithCollisions(new BABYLON.Vector3(
-          move.y * norm_c.x + move.x * norm_c.y,
-          this.gravity.y * elapsed_sec,
-          move.y * norm_c.y - move.x * norm_c.x
-        ))
-        if(move.length()){
-          let target_angle = Math.atan2(norm_c.x, norm_c.y); // orientation problem
-          let delta = target_angle - player.rotation.y;
-
-          if(delta < - Math.PI){
-            delta += Math.PI * 2;
-          } else if(delta > Math.PI){
-            delta = Math.PI * 2 - delta;
-          }
-
-          if(Math.abs(delta) > 0.5){
-            player.rotation.y = (player.rotation.y + Math.PI * 2 +
-                delta * elapsed_sec * 25) % (Math.PI * 2);
-          } else{
-            player.rotation.y = target_angle;
-          }
-
-          /*
-          player.lookAt(player.position.add(
-              c.multiplyByFloats(1.0, 0.0, 1.0)));
-          */
-        }
-
-        let camera = this.activeCamera;
-
-
-        let cameraOffsetX = 0.3;
-        let cameraOffsetZ = -2;
-        camera.position = player.position.add(new BABYLON.Vector3(
-            cameraOffsetZ * norm_c.x + cameraOffsetX * norm_c.y,
-            1.8,
-            cameraOffsetZ * norm_c.y - cameraOffsetX * norm_c.x
-        ));
-      }
-    });
   }
   removeEventListeners(){
     this.onKeyboardObservable.clear();
@@ -144,49 +85,17 @@ implements IScene
     shadowGenerator.blurKernel = 32;
 
 
-    Human.createHuman(this).then(human => {
+    this.player = await Human.createHuman(this).then(human => {
       human.addShadow(shadowGenerator);
       console.log("HUMAN:", human);
       GLOABL.set("human", human);
 
       human.meshes[0].name = "player";
       human.meshes[0].position.set(3, 3, 3);
-
-
-      let player = this.getMeshByName("player");
-      /*
-      if(!player.animations){
-        player.animations = [];
-      }
-      if(!player.animations[0]){
-        let orientation_animation = new BABYLON.Animation(
-          "orientationAnimation", "rotation.y", 60,
-          BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-          BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
-        );
-
-        player.animations.push(orientation_animation);
-        orientation_animation.enableBlending = true;
-
-      }
-      player.animations[0].setKeys([
-        {
-          frame: 0,
-          value: player.rotation.y,
-        }, {
-          frame: 60,
-          value: player.rotation.y + 3.14
-        }
-      ]);
-      */
+      return human;
     });
     
         
-    var helper = this.createDefaultEnvironment({
-        enableGroundShadow: true
-    });
-    helper.setMainColor(BABYLON.Color3.Gray());
-    helper.ground.position.y += 0.01;
 
     
     let sphere = Common.createSphere(this, {
@@ -194,13 +103,93 @@ implements IScene
       diameter: 2,
     })
     sphere.checkCollisions = true;
+    let done = true;
+    setTimeout(() => { done = false; }, 3000);
+    this.onBeforeRenderObservable.add((a, b) => {
+      if(!done){
+        done = true;
+        console.log(b);
+      }
+      sphere.moveWithCollisions(this.gravity);
+      box.moveWithCollisions(this.gravity);
+    })
 
     
     var ground = BABYLON.MeshBuilder.CreateGround("ground", {
-      height: 20, width: 20
+      height: 100, width: 100
     }, this);
     ground.position.set(0, -5, 0);
     ground.checkCollisions = true;
+
+    
+    let last_frame_time = performance.now();
+    this.onBeforeRenderObservable.add(() => {
+      let elapsed = performance.now() - last_frame_time;
+      let elapsed_sec = elapsed * 0.001;
+      last_frame_time = performance.now();
+
+      let c = this.activeCamera.getForwardRay().direction;
+      let norm_c = new BABYLON.Vector2(c.x, c.z).normalize();
+
+      let move = BABYLON.Vector2.Zero();
+      if(this.pressed.has("KeyA")){ move.x -= 1; }
+      if(this.pressed.has("KeyD")){ move.x += 1; }
+      if(this.pressed.has("KeyW")){ move.y += 1; }
+      if(this.pressed.has("KeyS")){ move.y -= 1; }
+      move.normalize().scaleInPlace(elapsed_sec * 5);
+
+
+      let player = this.getMeshByName("player");
+      if(player){
+        player.moveWithCollisions(new BABYLON.Vector3(
+          move.y * norm_c.x + move.x * norm_c.y,
+          this.gravity.y * elapsed_sec,
+          move.y * norm_c.y - move.x * norm_c.x
+        ))
+        
+        if(this.player){
+          if(move.length()){
+            this.player.beginWalk();
+          } else{
+            this.player.beginIdle();
+          }
+        }
+        if(move.length()){
+
+          let target_angle = Math.atan2(norm_c.x, norm_c.y); // orientation problem
+          let delta = target_angle - player.rotation.y;
+
+          if(delta < - Math.PI){
+            delta += Math.PI * 2;
+          } else if(delta > Math.PI){
+            delta = Math.PI * 2 - delta;
+          }
+
+          if(Math.abs(delta) > 0.5){
+            player.rotation.y = (player.rotation.y + Math.PI * 2 +
+                delta * elapsed_sec * 25) % (Math.PI * 2);
+          } else{
+            player.rotation.y = target_angle;
+          }
+
+          /*
+          player.lookAt(player.position.add(
+              c.multiplyByFloats(1.0, 0.0, 1.0)));
+          */
+        }
+
+        let camera = this.activeCamera;
+
+
+        let cameraOffsetX = 0.3;
+        let cameraOffsetZ = -2;
+        camera.position = player.position.add(new BABYLON.Vector3(
+            cameraOffsetZ * norm_c.x + cameraOffsetX * norm_c.y,
+            1.8,
+            cameraOffsetZ * norm_c.y - cameraOffsetX * norm_c.x
+        ));
+      }
+    });
 
     return this;
   }
